95e6f1a8772f27cd0d5c455b20962bfeb92848dd
Amdadul Sikder <amdadulsikder3@gmail.com>
2021-10-19 22:26:50 +0600

    responsive added

--- src/pages/Home/Appoinment/Appoinment.js
+++ src/pages/Home/Appoinment/Appoinment.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 
 const Appoinment = () => {
     const { serviceId } = useParams();
+
+    const [services, setservices] = useState([]);
+    // const [appoinment, setAppoinment] = useState({})
+
+    useEffect(() => {
+        fetch("/services.json")
+            .then(res => res.json())
+            .then(data => setservices(data))
+            .then(data => console.log(data))
+    }, [])
+
+    useEffect(() => {
+        const singleService = services.find(ss => ss.id === serviceId);
+        // setAppoinment(singleService);
+        console.log(singleService);
+
+    }, [])
+
     return (
         <div>
             <h3>This is appoinment {serviceId}</h3>
+            {/* <h3>This is  {appoinment?.name}</h3> */}
 
         </div>
     );
